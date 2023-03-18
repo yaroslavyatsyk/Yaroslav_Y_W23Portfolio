03cde25131d250cd1556d93991a2e8d2054de81b
Yaroslav9728 <yaroslav9728@gmail.com>
2023-03-17 22:47:48 -0400

    fixed about page

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
         <div className="text-center mb-5">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Yaroslav Yatsyk.
-            <br className="hidden lg:inline-block" />C#/.Net Developer
+            <br className="hidden lg:inline-block" />C#/.NET Developer
           </h1>
           <p className="mb-8 leading-relaxed">
           Treat people as they would treat you.
@@ -22,13 +22,6 @@ export default function About() {
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
               See My Projects
             </a>
-            <a href="https://www.linkedin.com/in/yaroslavyatsyk/">
-  <i class="fa fa-linkedin-square" style={{ fontSize: '36px' }}></i>
-</a>
-
-<a href="https://github.com/yaroslavyatsyk">
-<i class="fa fa-github" style={{fontSize: '36px'}}></i>
-</a>
           </div>
         </div>
       </div>
