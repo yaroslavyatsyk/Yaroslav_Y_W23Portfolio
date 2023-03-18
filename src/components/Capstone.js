c1ea7600a49c35f7c48bab666433aadef45080b9
Yaroslav9728 <yaroslav9728@gmail.com>
2023-03-18 17:47:22 -0400

    modified capstone

--- src/components/Capstone.js
+++ src/components/Capstone.js
@@ -48,11 +48,11 @@ export default function Capstone() {
     <tr style={{border: '1px solid white'}}>
       <th style={{border: '1px solid white'}}>Wireframes</th>
       <th style={{border: '1px solid white'}}><ul>
-        <li><img src="/Client Park Info.png"></img></li>
-        <li><img src="/Main Page Park List.png"></img></li>
-        <li><img src="/Client View Booking Details.png"/></li>
-        <li><img src="/Client Send Request for Park Website.png"/></li>
-        <li><img src="/Home Page.png"/></li>
+        <li><img src="/Client Park Info.png" height={50}></img></li>
+        <li><img src="/Main Page Park List.png" height={50}></img></li>
+        <li><img src="/Client View Booking Details.png" height={50}/></li>
+        <li><img src="/Client Send Request for Park Website.png" height={50}/></li>
+        <li><img src="/Home Page.png" height={50}/></li>
         </ul></th>
     </tr>
   </thead>
