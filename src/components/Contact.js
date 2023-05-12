f0cdb85fcd0653788cd23e057967ce863490dd9c
Yaroslav9728 <yaroslav9728@gmail.com>
2023-05-11 21:02:35 -0400

    added postal code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,7 +14,7 @@ export default function Contact() {
               </h2>
               <p className="mt-1">
                <br />
-                Toronto, Canada, Ontario
+                Toronto, Canada, Ontario, M9P 3S9
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
