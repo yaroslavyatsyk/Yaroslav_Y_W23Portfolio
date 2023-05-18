e28295bddab08cf57492417dfeca9c125e584d72
Yaroslav9728 <yaroslav9728@gmail.com>
2023-05-18 15:35:10 -0400

    Updated link for the resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,7 +9,7 @@ export default function Resume() {
             <br className="hidden lg:inline-block" />
           </h1>
 
-          <a className="text-indigo-400 leading-relaxed" href="https://www.dropbox.com/s/cp46qyysmcs0lt5/Yaroslav%20Yatsyk.%20.NET%20Developer.pdf?dl=1" download={true}><strong>Download Resume</strong></a>
+          <a className="text-indigo-400 leading-relaxed" href="https://www.dropbox.com/s/ys5q03l49auce18/Yaroslav%20Yatsyk%20.NET%20Developer.pdf?dl=1" download={true}><strong>Download Resume</strong></a>
 
 
 
