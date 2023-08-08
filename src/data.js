72ffcb552aa9d319b86e1a84c0ab844d5d962ce2
Yaroslav9728 <yaroslav9728@gmail.com>
2023-08-08 11:57:21 -0400

    fixed some errors

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Matrix Calculator App",
     subtitle: "",
     description:
-      "The Application demonstrates arithmetic operations om matrices, like: addiction, multiplication, and subtraction. \n Used Technologies: C# .Net, Windows Presentation Foundation (WPF)",
+      "The application demonstrates arithmetic operations om matrices, like: addition, multiplication, and subtraction. \n Used Technologies: C# .NET, Windows Presentation Foundation (WPF)",
     image: "../Project_1.png",
     link: "https://github.com/yaroslavyatsyk/Matrix-Calculator-App",
    
@@ -12,7 +12,7 @@ export const projects = [
     title: "Quadratic Equation App",
     subtitle: "",
     description:
-      "The App allows to students to ease the process of quadratic equation in Algebra course. It uses all rules of the quadratic equation solving\n Used Technologies: C# .Net, Windows Presentation Foundation (WPF)",
+      "The app allows to students to ease the learning process of quadratic equation in Algebra course. It uses all rules of the quadratic equation solving\n Used Technologies: C# .NET, Windows Presentation Foundation (WPF)",
     image: "../Project_2.png",
     link: "https://github.com/yaroslavyatsyk/Quadric-Equatation",
 
@@ -21,7 +21,7 @@ export const projects = [
     title: "Text Analyzer App",
     subtitle: "",
     description:
-      "The App accepts the text of any length, and analyzes in terms of language rules, like number of words, characters, appearance of unique and duplicate words\nUsed Technologies: C# .Net, Windows Presentation Foundation (WPF)",
+      "The app accepts the text of any length entered from a keyboard or read from a text file, and analyzes in terms of language rules, like number of words, characters, appearance of unique and duplicate words\nUsed Technologies: C# .NET, Windows Presentation Foundation (WPF)",
     image: "../Project_3.png",
     link: "https://github.com/yaroslavyatsyk/TextAnalyzerApp",
  
@@ -30,21 +30,21 @@ export const projects = [
     title: "SportsPro. Sporting Technician Management System",
     subtitle: "",
     description:
-      "The Web App with CRUD operations that allows to technician to manage data of sport facilities.\n Used Technology Stack: C# .NET, SQL, ASP.NET",
+      "The Web App with CRUD operations that allows to technician to manage data of sport facilities.\n Used Technology Stack: C# .NET, SQL, ASP.NET MVC",
     image: "../Project_4.png",
     link: "https://github.com/yaroslavyatsyk/SportsPro",
   },
   {
     title: "Number Analyzer App",
     subtitle: "",
-    description: "The App accepts the number of any length, and analyzes in terms of language rules, like number of digits, maximum and minimum digit, the frequence of digits in a specific number\nUsed Technologies: C# .Net, Windows Presentation Foundation (WPF)",
+    description: "The app accepts the number of any length, and analyzes in terms of language rules, like number of digits, maximum and minimum digit, the frequence of digits in a specific number\nUsed Technologies: C# .NET, Windows Presentation Foundation (WPF)",
     image: "../Project_5.png",
     link: "https://github.com/yaroslavyatsyk/Number-Analyser-App"
   },
   {
     title: "Fraction App",
     subtitle: "",
-    description: "The app allows to perform arithmetic operations on fractions, like: addiction, multiplication, and subtraction. \n Used Technologies: C# .Net, Windows Presentation Foundation (WPF)",
+    description: "The app allows to perform basic arithmetic operations on fractions, like: addition, multiplication, division, and subtraction. \n Used Technologies: C# .NET, Windows Presentation Foundation (WPF)",
     image: "../Project_6.png",
     link: "https://github.com/yaroslavyatsyk/Fraction-App" 
   }
@@ -101,6 +101,7 @@ export const skills = [
   "Object Oriented Programming (OOP)",
   ".NET Framework",
   "Entity Framework",
+  "Visual Studio Code"
   
   
 ];
