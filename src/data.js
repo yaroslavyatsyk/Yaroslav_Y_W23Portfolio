959811c06b4dae98ebc916c6be903b81b4bf1a04
Yaroslav9728 <yaroslav9728@gmail.com>
2023-08-09 22:03:13 -0400

    Update: Removed redundant things

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "",
     description:
       "The application demonstrates arithmetic operations om matrices, like: addition, multiplication, and subtraction. \n Used Technologies: C# .NET, Windows Presentation Foundation (WPF)",
-    image: "../Project_1.png",
+    image: "https://www.dropbox.com/scl/fi/54yoe05n5nxovavqdrghg/Project_1.png?rlkey=bx7mgfnmqdoca0y0ijx6bakq8&raw=1",
     link: "https://github.com/yaroslavyatsyk/Matrix-Calculator-App",
    
   },
@@ -13,7 +13,7 @@ export const projects = [
     subtitle: "",
     description:
       "The app allows to students to ease the learning process of quadratic equation in Algebra course. It uses all rules of solving a quadratic equation\n Used Technologies: C# .NET, Windows Presentation Foundation (WPF)",
-    image: "../Project_2.png",
+    image: "https://www.dropbox.com/scl/fi/54yoe05n5nxovavqdrghg/Project_2.png?rlkey=bx7mgfnmqdoca0y0ijx6bakq8&raw=1",
     link: "https://github.com/yaroslavyatsyk/Quadric-Equatation",
 
   },
@@ -22,7 +22,7 @@ export const projects = [
     subtitle: "",
     description:
       "The app accepts the text of any length entered from a keyboard or read from a text file, and analyzes in terms of language rules, like the number of words, characters, appearance of unique and/or duplicate words.\nUsed Technologies: C# .NET, Windows Presentation Foundation (WPF)",
-    image: "../Project_3.png",
+    image: "https://www.dropbox.com/scl/fi/54yoe05n5nxovavqdrghg/Project_3.png?rlkey=bx7mgfnmqdoca0y0ijx6bakq8&raw=1",
     link: "https://github.com/yaroslavyatsyk/TextAnalyzerApp",
  
   },
@@ -31,21 +31,21 @@ export const projects = [
     subtitle: "",
     description:
       "The Web App with CRUD operations that allows to technician to manage data of sport facilities.\n Used Technology Stack: C# .NET, SQL, ASP.NET MVC",
-    image: "../Project_4.png",
+    image: "https://www.dropbox.com/scl/fi/54yoe05n5nxovavqdrghg/Project_4.png?rlkey=bx7mgfnmqdoca0y0ijx6bakq8&dl=0",
     link: "https://github.com/yaroslavyatsyk/SportsPro",
   },
   {
     title: "Number Analyzer App",
     subtitle: "",
     description: "The app accepts the number of any length, and analyzes in terms of language rules, like number of digits, maximum and minimum digit, the frequence of digits in a specific number\nUsed Technologies: C# .NET, Windows Presentation Foundation (WPF)",
-    image: "../Project_5.png",
+    image: "https://www.dropbox.com/scl/fi/54yoe05n5nxovavqdrghg/Project_5.png?rlkey=bx7mgfnmqdoca0y0ijx6bakq8&raw=1",
     link: "https://github.com/yaroslavyatsyk/Number-Analyser-App"
   },
   {
     title: "Fraction App",
     subtitle: "",
     description: "The app allows to perform basic arithmetic operations on fractions, like: addition, multiplication, division, and subtraction. \n Used Technologies: C# .NET, Windows Presentation Foundation (WPF)",
-    image: "../Project_6.png",
+    image: "https://www.dropbox.com/scl/fi/54yoe05n5nxovavqdrghg/Project_6.png?rlkey=bx7mgfnmqdoca0y0ijx6bakq8&raw=1",
     link: "https://github.com/yaroslavyatsyk/Fraction-App" 
   }
 ];
