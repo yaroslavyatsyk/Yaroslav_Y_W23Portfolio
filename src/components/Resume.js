1c0aa776b1c6adc38b1fa69b54648b0c7f106cd7
Yaroslav9728 <yaroslav9728@gmail.com>
2023-11-04 22:30:11 -0400

    added some stuff to resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,7 +9,10 @@ export default function Resume() {
           </h1>
 
           <p>To download the resume, please use the link below.</p>
-
+          <br></br>
+          <p>Thank you!</p>
+          <br></br>
+          <p>Resume format: PDF</p>
           <a className="text-indigo-400 leading-relaxed" href="https://www.dropbox.com/scl/fi/y55bxqzzfs998x93bk73f/Yaroslav-Yatsyk.-.NET-Developer.pdf?rlkey=et5jn1l19no3a0auzwyyz8eol&dl=1" download={true}><strong>Download Resume</strong></a>
 
           </div>
