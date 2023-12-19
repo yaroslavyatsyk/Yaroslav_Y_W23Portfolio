50038925aa0b7b58d2138c4410c81f3728e4adb3
Yaroslav9728 <yaroslav9728@gmail.com>
2023-12-19 01:30:09 -0500

    minor improvements

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,14 +26,14 @@ export default function Projects() {
       className="sm:w-1/2 w-full p-4">
       <div className="relative w-1000 h-1000 flex-auto">
      
-      <br/>
+     
         <div className="relative inset-0 flex flex-col items-center justify-center p-4 bg-gray-900 bg-opacity-50 hover:bg-opacity-100">
           
           <h1 className="title-font text-lg font-small text-white mb-3">
             {project.title}
           </h1>
           <p className="leading-relaxed text-center font-small text-white mb-3">{project.description}</p>
-          <br/>
+        
         </div>
          
       <video preload="none" loop playsInline autoPlay muted className="w-2000 h-2000 object-cover object-center rounded flex-auto">
