1ad00092288872a6c572b66042055e9fa0793422
Yaroslav9728 <yaroslav9728@gmail.com>
2023-12-20 01:16:20 -0500

    minor improvements

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Matrix Calculator App",
     subtitle: "",
     description:
-      "The application demonstrates arithmetic operations om matrices, like: addition, multiplication, and subtraction. \n Used Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio",
+      "The application demonstrates arithmetic operations om matrices, like: addition, multiplication, and subtraction. \n Used Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio.",
     video: "https://www.dropbox.com/scl/fi/bi6t3y8o8j222b83r31p3/Matrix-Calculator-Demo.mp4?rlkey=ztrxz38j7d8e7wza8o5rgf5yi&raw=1",
     link: "https://github.com/yaroslavyatsyk/Matrix-Calculator-App",
    
@@ -12,7 +12,7 @@ export const projects = [
     title: "Quadratic Equation App",
     subtitle: "",
     description:
-      "The app allows to students to ease the learning process of quadratic equation in Algebra course. It uses all rules of solving a quadratic equation\n Used Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio",
+      "The app allows to students to ease the learning process of quadratic equation in Algebra course. It uses all rules of solving a quadratic equation.\n Used Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio.",
     video: "https://www.dropbox.com/scl/fi/5690o7r2b8i6vf7kbaeex/Quadratic-Equation-Demo.mp4?rlkey=mcb28p52igtnjkitl1j7lple6&raw=1",
     link: "https://github.com/yaroslavyatsyk/Quadric-Equatation",
 
@@ -21,7 +21,7 @@ export const projects = [
     title: "Text Analyzer App",
     subtitle: "",
     description:
-      "The app accepts the text of any length entered from a keyboard or read from a text file, and analyzes in terms of language rules, like the number of words, characters, appearance of unique and/or duplicate words.\nUsed Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio",
+      "The application accepts a text of any length, entered from a keyboard or read data from a text file, and conducts an analysis in terms of language rules, like the number of words, characters, appearance of unique and/or duplicate words.\nUsed Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio.",
     video: "https://www.dropbox.com/scl/fi/sy1dvlk0xmqbry11216zq/Text-Analyzer-Demo.mp4?rlkey=j7iuw1h15gy0eqdrzl3bgoyej&raw=1",
     link: "https://github.com/yaroslavyatsyk/TextAnalyzerApp",
  
@@ -30,28 +30,28 @@ export const projects = [
     title: "SportsPro. Sporting Technician Management System",
     subtitle: "",
     description:
-      "The Web App with CRUD operations that allows to technician to manage data of sport facilities.\n Used Technologies and Tools: C# .NET, SQL, ASP.NET MVC, Visual Studio, Microsoft SQL Server Management Studio, Microsoft SQL Server",
+      "The Web application that allows users to manage records of sport facilities.\n Used Technologies and Tools: C# .NET, SQL, ASP.NET MVC, Visual Studio, Microsoft SQL Server Management Studio, Microsoft SQL Server.",
     video: "https://www.dropbox.com/scl/fi/q8qqfdspfxevym8wm8ku1/Sports-Pro-Demo.mp4?rlkey=dmxg12oob67tlavc6z7hco8xw&raw=1",
     link: "https://github.com/yaroslavyatsyk/SportsPro",
   },
   {
     title: "Number Analyzer App",
     subtitle: "",
-    description: "The app accepts the number of any length, and analyzes in terms of language rules, like number of digits, maximum and minimum digit, the frequence of digits in a specific number\nUsed Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio",
+    description: "The application accepts a number, and conducts an analysis in terms of language rules, such as a number of digits, maximum and/or minimum digit of a particular number, the frequency of digits of a specific number.\nUsed Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio.",
     video: "https://www.dropbox.com/scl/fi/rhlsayiero51yki07donr/Number-Analysis-Demo.mp4?rlkey=fj3m4vbdmhvjgqc0u5mat7jkh&raw=1",
     link: "https://github.com/yaroslavyatsyk/Number-Analyser-App"
   },
   {
     title: "Fraction App",
     subtitle: "",
-    description: "The app allows to perform basic arithmetic operations on fractions, like: addition, multiplication, division, and subtraction. \n Used Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio",
+    description: "The application allows to perform basic arithmetic operations on fractions, such as addition, multiplication, division, and subtraction. \n Used Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio.",
     video: "https://www.dropbox.com/scl/fi/emmqopuog2n75slzoxip6/Fraction-App-Demo.mp4?rlkey=52gjjdk16dgv52ykrbtmvt18l&raw=1",
     link: "https://github.com/yaroslavyatsyk/Fraction-App" 
   },
   {
     title: "Assignment Management System",
     subtitle: "",
-    description: "The Web App with CRUD operations that allows to users to manage their assignments or tasks.\n Used Technologies and Tools: C# .NET, SQL, ASP.NET MVC, Visual Studio, Microsoft SQL Server Management Studio, Microsoft SQL Server",
+    description: "The web application that allows users to manage their assignments or tasks.\n Used Technologies and Tools: C# .NET, SQL, ASP.NET MVC, Visual Studio, Microsoft SQL Server Management Studio, Microsoft SQL Server.",
     link: "https://github.com/yaroslavyatsyk/ToDo-Web-App",
     video: "https://www.dropbox.com/scl/fi/ccf41kcgbhztxlbpmhsrr/Assignment-Management-System-Demo.mp4?rlkey=c7xgudyw47fr98ti221z6y8np&raw=1"
   }
