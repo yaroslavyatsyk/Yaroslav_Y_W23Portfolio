3e511a471c0ac6e3f80bc0015b575f656d77081d
Yaroslav9728 <yaroslav9728@gmail.com>
2024-02-08 15:27:13 -0500

    added database section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -46,6 +46,17 @@ export default function Skills() {
           ))}
         </div>
         <div className="text-center">
+          <h3 className="text-white">Databases</h3>
+          </div>
+          <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+            <div className="p-2 sm:w-1/2 w-full">
+              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                <span className="title-font font-medium text-white">
+                  Microsoft SQL Server
+                </span>
+              </div>
+        <div className="text-center">
         <h3 className="text-white">Tools</h3 >
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
