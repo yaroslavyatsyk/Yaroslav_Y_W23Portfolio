03397d251148e0b5e9d4047e319bcd79316bb434
Yaroslav9728 <yaroslav9728@gmail.com>
2024-05-28 16:33:22 -0400

    Updated Work Experience's and Projects' sections

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Matrix Calculator App",
     subtitle: "",
     description:
-      "The application demonstrates arithmetic operations on matrices, such as addition, multiplication, and subtraction. \n Used Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio, Git, GitHub.",
+      "Engineered Windows GUI application to demonstrate the concepts of matrices, including arithmetic operations, inverse, determinant, and transpose using C#, .NET, WPF, Visual Studio, Git, GitHub.",
     video: "https://www.dropbox.com/scl/fi/bi6t3y8o8j222b83r31p3/Matrix-Calculator-Demo.mp4?rlkey=ztrxz38j7d8e7wza8o5rgf5yi&raw=1",
     link: "https://github.com/yaroslavyatsyk/Matrix-Calculator-App",
    
@@ -12,7 +12,7 @@ export const projects = [
     title: "Quadratic Equation App",
     subtitle: "",
     description:
-      "The app allows to students to ease the learning process of quadratic equation in Algebra course. It uses all rules of solving a quadratic equation.\n Used Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio, Git, GitHub.",
+      "Implemented a desktop-based application to solve quadratic equations using C#, .NET, WPF, Visual Studio, Git, GitHub.",
     video: "https://www.dropbox.com/scl/fi/5690o7r2b8i6vf7kbaeex/Quadratic-Equation-Demo.mp4?rlkey=mcb28p52igtnjkitl1j7lple6&raw=1",
     link: "https://github.com/yaroslavyatsyk/Quadric-Equatation",
 
@@ -21,7 +21,7 @@ export const projects = [
     title: "Text Analyzer App",
     subtitle: "",
     description:
-      "The application accepts a text of any length, entered from a keyboard or read data from a text file, and conducts an analysis in terms of language rules, like the number of words, characters, appearance of unique and/or duplicate words.\nUsed Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio, Git, GitHub.",
+      "Created a text analysis application providing semantics information about a word or text using C#, .NET, WPF, Visual Studio, Git.",
     video: "https://www.dropbox.com/scl/fi/sy1dvlk0xmqbry11216zq/Text-Analyzer-Demo.mp4?rlkey=j7iuw1h15gy0eqdrzl3bgoyej&raw=1",
     link: "https://github.com/yaroslavyatsyk/TextAnalyzerApp",
  
@@ -30,35 +30,35 @@ export const projects = [
     title: "SportsPro. Sporting Technician Management System",
     subtitle: "",
     description:
-      "The Web application that allows users to manage records of sport facilities.\n Used Technologies and Tools: C# .NET, SQL, ASP.NET MVC, Visual Studio, Microsoft SQL Server Management Studio, Microsoft SQL Server, Git, GitHub.",
+      "Constructed a web application for tracking sports facilities and technical cases using C#, .NET, ASP.NET MVC, SQL Server, Entity Framework.",
     video: "https://www.dropbox.com/scl/fi/q8qqfdspfxevym8wm8ku1/Sports-Pro-Demo.mp4?rlkey=dmxg12oob67tlavc6z7hco8xw&raw=1",
     link: "https://github.com/yaroslavyatsyk/SportsPro",
   },
   {
     title: "Number Analyzer App",
     subtitle: "",
-    description: "The application accepts a number, and conducts an analysis in terms of language rules, such as a number of digits, maximum and/or minimum digit of a particular number, the frequency of digits of a specific number.\nUsed Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio, Git, GitHub.",
+    description: "Built a desktop-based application to analyze numbers, including prime numbers, odd numbers, and even numbers using C#, .NET, WPF, Visual Studio, Git, GitHub.",
     video: "https://www.dropbox.com/scl/fi/rhlsayiero51yki07donr/Number-Analysis-Demo.mp4?rlkey=fj3m4vbdmhvjgqc0u5mat7jkh&raw=1",
     link: "https://github.com/yaroslavyatsyk/Number-Analyser-App"
   },
   {
     title: "Fraction App",
     subtitle: "",
-    description: "The application allows to perform basic arithmetic operations on fractions, such as addition, multiplication, division, and subtraction. \n Used Technologies and Tools: C# .NET, Windows Presentation Foundation (WPF), Visual Studio, Git, GitHub.",
+    description: "Developed a desktop application to perform arithmetic operations on fractions, including addition, subtraction, multiplication, and division using C#, .NET, WPF, Visual Studio, Git, GitHub.",
     video: "https://www.dropbox.com/scl/fi/emmqopuog2n75slzoxip6/Fraction-App-Demo.mp4?rlkey=52gjjdk16dgv52ykrbtmvt18l&raw=1",
     link: "https://github.com/yaroslavyatsyk/Fraction-App" 
   },
   {
     title: "Assignment Management System",
     subtitle: "",
-    description: "The web application that allows users to manage their assignments or tasks.\n Used Technologies and Tools: C# .NET, SQL, ASP.NET MVC, Visual Studio, Microsoft SQL Server Management Studio, Microsoft SQL Server, Git, GitHub.",
+    description: "Developed a web application for task categorization and management using C#, .NET, ASP.NET MVC, Git, SQL Server, Entity Framework.",
     link: "https://github.com/yaroslavyatsyk/ToDo-Web-App",
     video: "https://www.dropbox.com/scl/fi/ccf41kcgbhztxlbpmhsrr/Assignment-Management-System-Demo.mp4?rlkey=c7xgudyw47fr98ti221z6y8np&raw=1"
   },
   {
     title: "Complex Number App",
     subtitle: "",
-    description: "The desktop application that allows people to get familiar with the concepts of complex numbers and perform basic arithmetic operations on complex numbers, such as addition, multiplication, division, and subtraction. \n Used Technologies and Tools: C# .NET, Universal Windows Presentation (UWP), Visual Studio, Git, GitHub.",
+    description: "A desktop-based applications demonstrates the concepts of complex numbers, including their properties and arithmetic operations using C#, .NET, WPF, Visual Studio, Git, GitHub.",
     link: "https://github.com/yaroslavyatsyk/Complex-Number-App",
     video: "https://www.dropbox.com/scl/fi/vj9czqp1knflfrwzxus2s/Complex-Number-Demo.mp4?rlkey=wyg7jziancs00y38dr4xn31vw&raw=1"
   }
@@ -75,11 +75,10 @@ export const workexperiences = [
     title: ".NET Developer Intern",
     company: "Postalgia Ink",
     descriptions: [
-      "Developed a character tracing module using C#, .NET, and Visual Studio, resulting in a 10% enhancement in data processing speed for hand-written letters.",
-      "Implemented Git for effective code source control and modification tracking, resulting in a 15% increase in software development speed within the team.",
-      "Implemented Gmail for sufficient reporting and communication with team members.",
-      "Followed all stages of the Software Development Life Cycle (SDLC) including gathering requirements and analysis, design, implementation, testing, maintenance, and support."
-
+      "Developed a character tracing module using C#, .NET, and Visual Studio, enhancing data processing speed by 10%.",
+      "Implemented Git for effective source control, increasing development speed by 15%.",
+      "Utilized Gmail for reporting and communication with team members.",
+      "Followed all stages of the Software Development Life Cycle (SDLC) including requirements gathering, design, implementation, testing, maintenance, and support."
 
 ],
     date: "March 2020 - December 2020",
@@ -88,11 +87,11 @@ export const workexperiences = [
     title: ".NET Developer Intern",
     company: "Linkup Studio",
     descriptions: [
-      "Developed a web application for an online store using ASP.NET MVC, resulting in a 25% profit increase.",
-      "Followed all stages of the Software Development Life Cycle (SDLC) including gathering requirements and analysis, design, implementation, testing, maintenance, and support.",
-      "Employed SQL Server Management Studio to design a robust database infrastructure for the application.",
-      "Implemented Skype to communicate with team members effectively.",
-      "Implemented Microsoft Office to create critical documentation with project description."
+      "Built a web application for an online store using ASP.NET MVC, increasing profits by 25%.",
+      "Complied with all phases of the SDLC.",
+      "Engineered a robust database using SQL Server Management Studio.",
+      "Executed Skype to communicate with team members effectively.",
+      "Created critical documentation using Microsoft Office."
     ],
     date: "January 2019 - July 2019"
   },
@@ -100,11 +99,11 @@ export const workexperiences = [
     title: ".NET Developer Intern",
     company: "Sigma Software Group",
     descriptions: [
-      "Developed a web application for a team management system using ASP.NET MVC, resulting in a 10% increase in teamwork efficiency.",
-      "Utilized SQL Server Management Studio to design a comprehensive database for the project.",
-      "Employed Team Foundation Server to track and manage changes in the project.",
-      "Implemented Skype to communicate with team members effectively.",
-      "Followed all stages of the Software Development Life Cycle (SDLC) including gathering requirements and analysis, design, implementation, testing, maintenance, and support."
+      "Designed a web application for a team management system using ASP.NET MVC, improving teamwork efficiency by 10%.",
+      "Designed a comprehensive database using SQL Server Management Studio.",
+      "Managed changes with Team Foundation Server",
+      "Communicated with team members via Skype.",
+      "Adhered to the SDLC procedures."
     ],
     date: "May 2018 - August 2018",
   },
