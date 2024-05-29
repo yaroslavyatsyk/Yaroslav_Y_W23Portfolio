60b7ac69592abfaba3154647755041b39e394727
Yaroslav9728 <yaroslav9728@gmail.com>
2024-05-29 15:10:17 -0400

    Minor changes

--- src/data.js
+++ src/data.js
@@ -12,7 +12,7 @@ export const projects = [
     title: "Quadratic Equation App",
     subtitle: "",
     description:
-      "Implemented a desktop-based application to solve quadratic equations using C#, .NET, WPF, Visual Studio, Git, GitHub.",
+      "Implemented desktop-based application to solve quadratic equations using C#, .NET, WPF, Visual Studio, Git, GitHub.",
     video: "https://www.dropbox.com/scl/fi/5690o7r2b8i6vf7kbaeex/Quadratic-Equation-Demo.mp4?rlkey=mcb28p52igtnjkitl1j7lple6&raw=1",
     link: "https://github.com/yaroslavyatsyk/Quadric-Equatation",
 
@@ -21,7 +21,7 @@ export const projects = [
     title: "Text Analyzer App",
     subtitle: "",
     description:
-      "Created a text analysis application providing semantics information about a word or text using C#, .NET, WPF, Visual Studio, Git.",
+      "Created text analysis application providing semantics information about a word or text using C#, .NET, WPF, Visual Studio, Git.",
     video: "https://www.dropbox.com/scl/fi/sy1dvlk0xmqbry11216zq/Text-Analyzer-Demo.mp4?rlkey=j7iuw1h15gy0eqdrzl3bgoyej&raw=1",
     link: "https://github.com/yaroslavyatsyk/TextAnalyzerApp",
  
@@ -30,35 +30,35 @@ export const projects = [
     title: "SportsPro. Sporting Technician Management System",
     subtitle: "",
     description:
-      "Constructed a web application for tracking sports facilities and technical cases using C#, .NET, ASP.NET MVC, SQL Server, Entity Framework.",
+      "Constructed web application for tracking sports facilities and technical cases using C#, .NET, ASP.NET MVC, SQL Server, Entity Framework.",
     video: "https://www.dropbox.com/scl/fi/q8qqfdspfxevym8wm8ku1/Sports-Pro-Demo.mp4?rlkey=dmxg12oob67tlavc6z7hco8xw&raw=1",
     link: "https://github.com/yaroslavyatsyk/SportsPro",
   },
   {
     title: "Number Analyzer App",
     subtitle: "",
-    description: "Built a desktop-based application to analyze numbers, including prime numbers, odd numbers, and even numbers using C#, .NET, WPF, Visual Studio, Git, GitHub.",
+    description: "Built desktop-based application to analyze numbers, including prime numbers, odd numbers, and even numbers using C#, .NET, WPF, Visual Studio, Git, GitHub.",
     video: "https://www.dropbox.com/scl/fi/rhlsayiero51yki07donr/Number-Analysis-Demo.mp4?rlkey=fj3m4vbdmhvjgqc0u5mat7jkh&raw=1",
     link: "https://github.com/yaroslavyatsyk/Number-Analyser-App"
   },
   {
     title: "Fraction App",
     subtitle: "",
-    description: "Developed a desktop application to perform arithmetic operations on fractions, including addition, subtraction, multiplication, and division using C#, .NET, WPF, Visual Studio, Git, GitHub.",
+    description: "Developed desktop application to perform arithmetic operations on fractions, including addition, subtraction, multiplication, and division using C#, .NET, WPF, Visual Studio, Git, GitHub.",
     video: "https://www.dropbox.com/scl/fi/emmqopuog2n75slzoxip6/Fraction-App-Demo.mp4?rlkey=52gjjdk16dgv52ykrbtmvt18l&raw=1",
     link: "https://github.com/yaroslavyatsyk/Fraction-App" 
   },
   {
     title: "Assignment Management System",
     subtitle: "",
-    description: "Developed a web application for task categorization and management using C#, .NET, ASP.NET MVC, Git, SQL Server, Entity Framework.",
+    description: "Developed web application for task categorization and management using C#, .NET, ASP.NET MVC, Git, SQL Server, Entity Framework.",
     link: "https://github.com/yaroslavyatsyk/ToDo-Web-App",
     video: "https://www.dropbox.com/scl/fi/ccf41kcgbhztxlbpmhsrr/Assignment-Management-System-Demo.mp4?rlkey=c7xgudyw47fr98ti221z6y8np&raw=1"
   },
   {
     title: "Complex Number App",
     subtitle: "",
-    description: "A desktop-based applications demonstrates the concepts of complex numbers, including their properties and arithmetic operations using C#, .NET, WPF, Visual Studio, Git, GitHub.",
+    description: "Desktop-based application demonstrates the concepts of complex numbers, including their properties and arithmetic operations using C#, .NET, WPF, Visual Studio, Git, GitHub.",
     link: "https://github.com/yaroslavyatsyk/Complex-Number-App",
     video: "https://www.dropbox.com/scl/fi/vj9czqp1knflfrwzxus2s/Complex-Number-Demo.mp4?rlkey=wyg7jziancs00y38dr4xn31vw&raw=1"
   }
@@ -75,10 +75,10 @@ export const workexperiences = [
     title: ".NET Developer Intern",
     company: "Postalgia Ink",
     descriptions: [
-      "Developed a character tracing module using C#, .NET, and Visual Studio, enhancing data processing speed by 10%.",
+      "Developed character tracing module using C#, .NET, and Visual Studio, enhancing data processing speed by 10%.",
       "Implemented Git for effective source control, increasing development speed by 15%.",
       "Utilized Gmail for reporting and communication with team members.",
-      "Followed all stages of the Software Development Life Cycle (SDLC) including requirements gathering, design, implementation, testing, maintenance, and support."
+      "Followed all stages of Software Development Life Cycle (SDLC) including requirements gathering, design, implementation, testing, maintenance, and support."
 
 ],
     date: "March 2020 - December 2020",
@@ -87,9 +87,9 @@ export const workexperiences = [
     title: ".NET Developer Intern",
     company: "Linkup Studio",
     descriptions: [
-      "Built a web application for an online store using ASP.NET MVC, increasing profits by 25%.",
-      "Complied with all phases of the SDLC.",
-      "Engineered a robust database using SQL Server Management Studio.",
+      "Built web application for online store using ASP.NET MVC, increasing profits by 25%.",
+      "Complied with all phases of SDLC.",
+      "Engineered robust database using SQL Server Management Studio.",
       "Executed Skype to communicate with team members effectively.",
       "Created critical documentation using Microsoft Office."
     ],
@@ -99,11 +99,11 @@ export const workexperiences = [
     title: ".NET Developer Intern",
     company: "Sigma Software Group",
     descriptions: [
-      "Designed a web application for a team management system using ASP.NET MVC, improving teamwork efficiency by 10%.",
-      "Designed a comprehensive database using SQL Server Management Studio.",
+      "Designed web application for team management system using ASP.NET MVC, improving teamwork efficiency by 10%.",
+      "Designed comprehensive database using SQL Server Management Studio.",
       "Managed changes with the Team Foundation Server",
       "Communicated with team members via Skype.",
-      "Adhered to the SDLC procedures."
+      "Adhered to SDLC procedures."
     ],
     date: "May 2018 - August 2018",
   },
