d57335b6c1e9f56f3c7ac6762e3b58100d0af4d1
Yaroslav Yatsyk <31794068+yaroslavyatsyk@users.noreply.github.com>
2025-04-06 19:14:21 -0400

    updates in project's data

--- src/data.js
+++ src/data.js
@@ -43,7 +43,7 @@ export const projects = [
   },
   {
     title: "C# For Everybody",
-    description: "This is a social project where developers of all seniority levels can contribute their sets of coding challenges to help beginners enhance their coding and problem-solving skills.",
+    description: "This is a social project where developers of all seniority levels can contribute their sets of coding challenges to help beginners enhance their coding, analytical, and problem-solving skills. Used technologies: C#, .NET, and Git.",
     photo: "https://www.dropbox.com/scl/fi/1d515hf3vim6764202a6v/Screenshot-2024-10-24-162526.png?rlkey=7j9ke5scc0c1lbf83u389oitv&st=dj6br4hl&raw=1",
     link: "https://github.com/Kalutu/csharp-for-everybody"
   }
