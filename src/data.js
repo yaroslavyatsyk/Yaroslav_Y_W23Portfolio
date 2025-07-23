065d947c2b08b18d097ab8778d29ca8eb718b567
Yaroslav Yatsyk <31794068+yaroslavyatsyk@users.noreply.github.com>
2025-07-22 20:28:30 -0400

    updated data for skills

--- src/data.js
+++ src/data.js
@@ -49,9 +49,9 @@ export const projects = [
   }
 ];
 export const  programmingLanguages = ["C#", "SQL","Java"];
-export const frameworks = ["ASP.NET MVC", "Entity Framework", ".NET Core", ".NET Framework", "Windows Forms", "Windows Presentation Foundation (WPF)","NUnit Testing Framework"];
+export const frameworks = ["ASP.NET MVC", "Entity Framework", ".NET Core", ".NET Framework", "Windows Forms", "Windows Presentation Foundation (WPF)","NUnit Testing Framework","LINQ"];
 export const tools = ["Visual Studio", "Visual Studio Code", "Git", "GitHub", "Microsoft SQL Server Management Studio", "UML (Unified Modeling Language)", "Vercel", "Azure Cloud","Postman","Docker","ChatGPT","Copilot","Inteliji IDEA"];
-export const others = ["Agile Scrum", "Software Development Life Cycle (SDLC)","JSON","Object-Oriented Programming (OOP)", "Test Driven Development (TDD)", "Waterfall Model"];
+export const others = ["Agile Scrum", "Software Development Life Cycle (SDLC)","JSON","Object-Oriented Programming (OOP)", "Test Driven Development (TDD)", "Waterfall Model", "Unit Testing", 'DevOps'];
 export const databases = ["Microsoft SQL Server", "MySQL", "SQLite"];
 export const frontend = ["HTML", "CSS","Tailwind CSS", "JavaScript","Razor","JQuery","React"];
 
