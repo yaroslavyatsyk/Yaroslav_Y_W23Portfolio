489c80543b3f0939702832d08d1709321e002ca7
Yaroslav Yatsyk <31794068+yaroslavyatsyk@users.noreply.github.com>
2026-06-04 17:04:31 -0400

    syntax error fixed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
-import About_Me from "./components/AboutMe";
+import AboutMe from "./components/About";
 import {useState} from 'react';
 import './index.css';
 
