121a358121405e4b8a2019306ffc6fcf46715f3a
Yaroslav Yatsyk <31794068+yaroslavyatsyk@users.noreply.github.com>
2026-06-04 17:02:05 -0400

    changed name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
-import About_Me from "./components/About me";
+import About_Me from "./components/AboutMe";
 import {useState} from 'react';
 import './index.css';
 
